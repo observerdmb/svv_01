2dd92601012f01bbb9d727ed99a20f1a5d407d79
Vasiliy <Vasyarus@inbox.ru>
2018-06-07 18:18:04 +0300

    New login style

--- accounts/urls.py
+++ accounts/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     url(r'^main$', views.main, name='main_page'),
     url('^not_exists/', views.empty_view, name='empty_page'),
     url('^login/', views.login_user, name='log-in'),
-    url('^logout/', logout, {'template_name': 'empty.html', 'next_page': '/login'}, name='logout'),
+    url('^logout/', logout, {'template_name': 'empty.html', 'next_page': '/main'}, name='logout'),
     url(r'^accounts/register', RegistrationView.as_view(form_class=AccountRegistrationForm), name='registration.register'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^main/edit_account', views.edit_account, name='edit_page'),
